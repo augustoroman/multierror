425846fd6f08bd22b93bb1bd326d3402421b495d
Augusto Roman <aroman@vannevartech.com>
2013-08-30 00:35:28 -0700

    Add docs

--- multierror.go
+++ multierror.go
@@ -1,3 +1,4 @@
+// Package multierror defines an error Accumulator to contain multiple errors.
 package multierror
 
 import (
@@ -5,13 +6,24 @@ import (
 	"fmt"
 )
 
-type MultiError []error
+// Accumulator is an error accumulator.
+//
+// Usage:
+//
+//     var errors multierror.Accumulator
+//     errors.Push(returnsErrOrNil())
+//     errors.Push(returnsErrOrNil())
+//     errors.Push(returnsErrOrNil())
+//     return errors.Error()
+type Accumulator []error
 
-func (m *MultiError) Push(err error) {
+// Push adds an error to the Accumulator.  If err is nil, then Accumulator is
+// not affected.
+func (m *Accumulator) Push(err error) {
 	if err == nil {
 		return
 	}
-	// Check for a multierror
+	// Check for a Accumulator
 	if e, ok := err.(_error); ok {
 		*m = append(*m, e...)
 		return
@@ -20,23 +32,28 @@ func (m *MultiError) Push(err error) {
 	*m = append(*m, err)
 }
 
-func (m *MultiError) Pushf(fmtstr string, args ...interface{}) {
+// Pushf adds a formatted error string to the Accumulator.  It is a shortcut
+// for Push(fmt.Error(...)).
+func (m *Accumulator) Pushf(fmtstr string, args ...interface{}) {
 	*m = append(*m, fmt.Errorf(fmtstr, args...))
 }
 
-func (m *MultiError) Error() error {
+// Error returns the accumulated errors.  If no errors have been pushed onto the
+// accumulator, then nil will be returned.
+func (m *Accumulator) Error() error {
 	if len(*m) == 0 {
 		return nil
 	}
 	return _error(*m)
 }
 
-func (m MultiError) String() string {
+// String prints the accumulated errors or "nil" if no errors have been pushed.
+func (m Accumulator) String() string {
 	return _error(m).Error()
 }
 
-// This type implements the error interface.  This is separate from MultiError
-//
+// This type implements the actual error interface.  This is separate from
+// Accumulator so to avoid accidentally returning an interface to a nil pointer.
 type _error []error
 
 func (m _error) Error() string {
