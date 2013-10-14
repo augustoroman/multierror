657cca1c8dc7727cfc01d9296e3cdc522608671b
Augusto Roman <aroman@vannevartech.com>
2013-10-14 16:39:37 -0700

    Fix test.

--- multierror_test.go
+++ multierror_test.go
@@ -8,69 +8,69 @@ import (
 func Works() error { return nil }
 func Fails() error { return fmt.Errorf("Failed") }
 
-func FailMulti(msg string, n int) error {
-	var e MultiError
+func FailAccumulator(msg string, n int) error {
+	var e Accumulator
 	for i := 0; i < n; i++ {
 		e.Pushf("%s%d", msg, i+1)
 	}
 	return e.Error()
 }
 
-func ReturnsEmptyMultiError() error {
-	var e MultiError
+func ReturnsEmptyAccumulator() error {
+	var e Accumulator
 	e.Push(Works())
 	e.Push(Works())
 	return e.Error()
 }
 
-func TestEmptyMultiError(t *testing.T) {
-	var e MultiError
+func TestEmptyAccumulator(t *testing.T) {
+	var e Accumulator
 	e.Push(Works())
 	e.Push(Works())
 	if e != nil {
-		t.Fatal("Empty MultiError is not nil")
+		t.Fatal("Empty Accumulator is not nil")
 	}
 
-	if ReturnsEmptyMultiError() != nil {
-		t.Fatal("Empty MultiError return value is not nil")
+	if ReturnsEmptyAccumulator() != nil {
+		t.Fatal("Empty Accumulator return value is not nil")
 	}
 }
 
-func ReturnsNonEmptyMultiError() error {
-	var e MultiError
+func ReturnsNonEmptyAccumulator() error {
+	var e Accumulator
 	e.Push(Fails())
 	e.Push(Fails())
 	return e.Error()
 }
 
-func TestNonEmptyMultiError(t *testing.T) {
-	var e MultiError
+func TestNonEmptyAccumulator(t *testing.T) {
+	var e Accumulator
 	e.Push(Fails())
 	e.Push(Fails())
 	if e == nil {
-		t.Fatal("Non-empty MultiError is nil")
+		t.Fatal("Non-empty Accumulator is nil")
 	}
 
-	if ReturnsNonEmptyMultiError() == nil {
-		t.Fatal("Non-empty MultiError return value is nil")
+	if ReturnsNonEmptyAccumulator() == nil {
+		t.Fatal("Non-empty Accumulator return value is nil")
 	}
 }
 
-func TestPushingMultiError(t *testing.T) {
-	var e MultiError
-	e.Push(FailMulti("Fail", 2))
+func TestPushingAccumulator(t *testing.T) {
+	var e Accumulator
+	e.Push(FailAccumulator("Fail", 2))
 	if fmt.Sprint(e) != `2 errors: ["Fail1", "Fail2"]` {
 		t.Fatal("Incorrect error string: ", e)
 	}
 
-	e.Push(FailMulti("X", 3))
+	e.Push(FailAccumulator("X", 3))
 	if fmt.Sprint(e) != `5 errors: ["Fail1", "Fail2", "X1", "X2", "X3"]` {
 		t.Fatal("Incorrect error string: ", e)
 	}
 }
 
-func TestMultiErrorStringification(t *testing.T) {
-	var e MultiError
+func TestAccumulatorStringification(t *testing.T) {
+	var e Accumulator
 	if fmt.Sprint(e) != `nil` {
 		t.Fatal("Incorrect error string: ", e)
 	}
@@ -85,7 +85,7 @@ func TestMultiErrorStringification(t *testing.T) {
 		t.Fatal("Incorrect error string: ", e)
 	}
 
-	if ReturnsNonEmptyMultiError().Error() != `2 errors: ["Failed", "Failed"]` {
-		t.Fatal("Incorrect error string: ", ReturnsNonEmptyMultiError())
+	if ReturnsNonEmptyAccumulator().Error() != `2 errors: ["Failed", "Failed"]` {
+		t.Fatal("Incorrect error string: ", ReturnsNonEmptyAccumulator())
 	}
 }
